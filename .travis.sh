304f69b2cd3362b855fc18d116757277509361ef
Scott Sandler <ssandler@slack-corp.com>
2019-07-23 16:46:58 -0700

    travis config

---
+++ .travis.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -ex
+apt update -y
+DEBIAN_FRONTEND=noninteractive apt install -y php-cli zip unzip
+hhvm --version
+php --version
+
+(
+  cd $(mktemp -d)
+  curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+)
+if (hhvm --version | grep -q -- -dev); then
+  rm -f composer.lock
+fi
+composer install
+
+hh_client
+
+vendor/bin/hacktest tests/
+
+vendor/bin/hhast-lint
