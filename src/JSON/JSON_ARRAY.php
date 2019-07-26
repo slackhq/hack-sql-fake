f6815f7cd381d0336a0728778cf175b4c401c36d
Lexidor Digital <31805625+Lexidor@users.noreply.github.com>
2019-07-26 04:31:16 +0200

    Defer JSON_ARRAY and format hhconfig

---
+++ src/JSON/JSON_ARRAY.php
@@ -0,0 +1,16 @@
+<?hh // strict
+
+// The function names and API's are not final.
+// They are just implementations.
+// They will get a better name when completed.
+
+namespace Slack\SQLFake\JSON;
+
+use namespace HH\Lib\C;
+
+/**
+ * @reference https://dev.mysql.com/doc/refman/5.7/en/json-creation-functions.html#function_json-array
+ * @TODO Support the second argument `path`
+ */
+<<__Deprecated('Use an array intrinsic expression')>>
+function array_(null $_): void {}
