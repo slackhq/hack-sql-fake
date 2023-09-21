047afaa4324ca94df41c16dc56a0bb5fdbb9f216
Priya Bibra <pbibra@slack-corp.com>
2023-09-21 13:28:49 -0700

    add AsyncMysqlQueryErrorResult and SQLFakeAsyncMysqlException

--- src/Exception.php
+++ src/Exception.php
@@ -8,3 +8,11 @@ final class SQLFakeParseException extends SQLFakeException {}
 final class SQLFakeRuntimeException extends SQLFakeException {}
 final class SQLFakeUniqueKeyViolation extends SQLFakeException {}
 final class SQLFakeVitessQueryViolation extends SQLFakeException {}
+final class SQLFakeAsyncMysqlException extends SQLFakeException {
+	public function __construct(private \AsyncMysqlErrorResult $result) {
+		parent::__construct();
+	}
+	public function getResult(): \AsyncMysqlErrorResult {
+		return $this->result;
+	}
+}
